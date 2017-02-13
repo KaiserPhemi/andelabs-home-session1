279447f35430345d544b0f4de524f85b24b233ab
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-13 10:40:36 +0100

    getPrimes modified and new test cases added

--- andelabs-q2/app/getPrimes.js
+++ andelabs-q2/app/getPrimes.js
@@ -3,9 +3,16 @@
 
 	module.exports ={
 		getPrimes : function(integerN){
-			var makePrimes = (typeof integerN != 'number') ? "Input must be a number": function(integerN){
-
+			var makePrimes =(integerN <= 0)? "Input must be a number and greater than zero": 
+							(typeof integerN != 'number') ? "Input must be a number": function(integerN){
+								
+								var primesList = [0, 1];
+								for(var count =2; count < integerN; count++ ){
+									var resultList = !(integerN % count == 0) ? primesList.push(count) : primesList;
+									return resultList;
+				}
 			};
+
 			return makePrimes;
 		}
 	};
--- andelabs-q2/spec/getPrimes-spec.js
+++ andelabs-q2/spec/getPrimes-spec.js
@@ -5,24 +5,24 @@ describe("Test for all types of input", function(){
 	
 	describe("Test for numeric input ", function(){
 	
-		it("Should return [0, 1, 2, 3] for numeric input 3", function(){
-			expect(getPrimes(3)).toBe([0, 1, 2, 3]);
+		it("Should return [1, 2, 3] for numeric input 3", function(){
+			expect(getPrimes(3)).toBe([1, 2, 3]);
 		});
 
-		it("Should return [0, 1, 2, 3, 5, 7] for numeric input 7", function(){
-			expect(getPrimes(7)).toBe([0, 1, 2, 3]);
+		it("Should return [1, 2, 3, 5, 7] for numeric input 7", function(){
+			expect(getPrimes(7)).toBe([1, 2, 3, 5, 7]);
 		});
 
-		it("Should return [0, 1, 2, 3] for numeric input 3", function(){
-			expect(getPrimes(3)).toBe([0, 1, 2, 3, 5, 7]);
+		it("Should return `Input must be a number and greater than zero` for input 0", function(){
+			expect(getPrimes(0)).toBe('Input must be a number and greater than zero');
 		});
 
-		it("Should return [0, 1, 2, 3, 5, 7, 11, 13, 17, 19] for numeric input 19", function(){
-			expect(getPrimes(19)).toBe([0, 1, 2, 3, 5, 7, 11, 13, 17, 19]);
+		it("Should return `Input must be a number and greater than zero` for input -5", function(){
+			expect(getPrimes(-5)).toBe('Input must be a number and greater than zero');
 		});
 
-		it("Should return [0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for numeric input 29", function(){
-			expect(getPrimes(29)).toBe([0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+		it("Should return [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29] for numeric input 29", function(){
+			expect(getPrimes(29)).toBe([1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
 		});
 		
 	});
