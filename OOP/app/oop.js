d0231590b96c8d8a59169ab400f0a9363e7f3248
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-13 13:40:15 +0100

    OOP Solved but still working on failures

--- OOP/app/oop.js
+++ OOP/app/oop.js
@@ -1,4 +1,20 @@
+/*IIFE implemented to prevent pollution of the global namespace*/
 (function(){
 	"use strict";
-	
+	module.exports = {
+
+		Animal : function(name, family, maximumSpeed, numOfLegs, habitat){
+
+			this.name = name;
+			this.family = family;
+			this.maximumSpeed = maximumSpeed;
+			this.numOfLegs = function(habitat){
+				var animalEnv = (habitat == 'Ground') ? 4 :
+								(habitat == 'Air') ? 2 :
+								(habitat == 'Water') ? 0 : "Animal habitat can only be `Ground`, `Air` or `Water` only";
+				return animalEnv;
+			};
+			
+		}
+	};
 })();
--- OOP/spec/oop-spec.js
+++ OOP/spec/oop-spec.js
@@ -1,59 +1,53 @@
 (function(){
   'use strict';
-  describe("Car Class: Create a car, make it drive", function() {
 
-    it("The car should be a type of `object`, and an instance of the `Car` class", function() {
-      var honda = new Car('Honda');
-      expect(typeof honda).toEqual(typeof {});
-      expect(honda instanceof Car).toBeTruthy();
+  var Animal = require("../app/oop").Animal;
+
+  describe("Animal Class: Create an animal", function() {
+
+    it("The animal should be a type of `object`, and an instance of the `Animal` class", function() {
+      var dog = Object.create(Animal);
+      expect(typeof dog).toEqual(typeof {});
+      expect(dog instanceof Animal).toBeTruthy();
     });
 
-    it("The car should be called 'General' if no name is passed as a parameter", function() {
-      var gm = new Car();
-      expect(gm.name).toEqual('General');
-      expect(gm.model).toBe('GM');
+    it("The animal habitat should be a property of the animal", function(){
+      var fish = Object.create(Animal);
+      expect(fish.habitat).toEqual('Water');
+      expect(fish.numOfLegs).toEqual(0);
     });
 
-    it("The car name and model should be a property of the car", function() {
-      var toyota  = new Car('Toyota', 'Corolla');
-      expect(toyota.name).toBe('Toyota');
-      expect(toyota.model).toBe('Corolla');
+    it("The animal should be called 'Generic' if no name is passed as a parameter", function() {
+      var wiered = Object.create(Animal);
+      expect(wiered.name).toEqual('Generic');
+      expect(wiered.family).toBe('GC');
     });
 
-    it("The car shoud have four (4) doors except its a Porshe or Koenigsegg", function() {
-      var opel  = new Car('Opel', 'Omega 3');
-      expect(opel.numOfDoors).toBe(4);
-
-      var porshe = new Car('Porshe', '911 Turbo');
-      expect(porshe.numOfDoors).toBe(2);
-      porshe.drive(5);
-      expect(porshe.speed).toBe('250 km/h');
-      expect((function(){return new Car('Koenigsegg', 'Agera R');}()).numOfDoors).toBe(2);
+    it("The animal maximum speed should be a property of the Animal", function() {
+      var horse  = Object.create(Animal);
+      expect(horse.name).toEqual('Shadowfax');
+      expect(horse.family).toBe('Herbivore');
+      expect(horse.maximumSpeed).toBe(45);
     });
 
-    it("The car shoud have four (4) wheels except its a type of trailer", function() {
-      var man  = new Car('MAN', 'Truck', 'trailer');
-      expect(man.numOfWheels).toBe(8);
-      expect(man.isSaloon).toBe(false);
-
-      var koenigsegg = new Car('Koenigsegg', 'Agera R');
-      expect(koenigsegg.numOfWheels).toBe(4);
-      expect(koenigsegg.isSaloon).toBeTruthy();
+    it("The animal name and family should be a property of the Animal", function() {
+      var cat  = Object.create(Animal);
+      expect(cat.name).toBe('Casper');
+      expect(cat.family).toBe('Carnivore');
     });
 
-    it("The Trailer should have speed 0 km/h until you put `the pedal to the metal`", function() {
-      var man  = new Car('MAN', 'Truck', 'trailer');
-      expect(man.speed).toBe('0 km/h');
-      man.drive(7);
-      expect(man.speed).toBe('77 km/h');
+    it("The Animal should have four (4) legs except its a habitat is Water or Air", function() {
+      var goat  = Object.create(Animal);
+      expect(goat.numOfLegs('Ground')).toBe(4);
+
+      var fish = Object.create(Animal);
+      expect(fish.numOfLegs('Water')).toBe(0);
+      
+
+      var eagle = Object.create(Animal);
+      expect(eagle.numOfLegs('Air')).toBe(2);
+           
     });
 
-    it("The car drive function should return the instance of the Car class", function() {
-      var man  = new Car('MAN', 'Truck', 'trailer');
-      var drivingMan = man.drive(7);
-      expect(drivingMan instanceof Car).toBeTruthy();
-      expect(typeof drivingMan.drive).toBe(typeof (function (){}));
-      expect(man.speed).toBe(drivingMan.speed);
-    });
   });
 })();
